3ef05f8b72fd738133d1533b355d88ced5544efb
nusratjahan2 <nusrathaque717@gmail.com>
2023-09-18 12:10:58 +0600

    rectanle calculation done

--- scripts/app.js
+++ scripts/app.js
@@ -7,9 +7,34 @@ document.getElementById('calculateTriangle').addEventListener('click' , function
     const showResult = document.getElementById('dynamicResults');
     const section = document.createElement('section');
     section.innerHTML = `
-       <ol>
-         <li>${area}</li>
-       </ol>
+       <ul class="calculationListStyling">
+         <li class="d-flex justify-content-between align-items-center">
+          <p>Triangle</p>
+          <p>${area}cm<sup>2</sup></p>
+          <button type="button" class="btn btn-primary">Convert to m<sup>2</sup></button>
+        </li>
+       </ul>
+    `
+    showResult.appendChild(section);
+})
+
+
+document.getElementById('calculateRectangle').addEventListener('click' , function(){
+    const width = document.getElementById('width').value;
+    const length = document.getElementById('length').value;
+    const area = width * length;
+
+    // show value in calculation area
+    const showResult = document.getElementById('dynamicResults');
+    const section = document.createElement('section');
+    section.innerHTML = `
+       <ul class="calculationListStyling">
+         <li class="d-flex justify-content-between align-items-center">
+          <p>Rectangle</p>
+          <p>${area}cm<sup>2</sup></p>
+          <button type="button" class="btn btn-primary">Convert to m<sup>2</sup></button>
+        </li>
+       </ul>
     `
     showResult.appendChild(section);
 })
