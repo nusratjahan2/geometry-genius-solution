6cfb46c77fc5a458a8a0a93d5a9974fa488f447a
nusratjahan2 <nusrathaque717@gmail.com>
2023-09-18 12:23:28 +0600

    parallelogram calculation done

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// --------Triangle--------
 document.getElementById('calculateTriangle').addEventListener('click' , function(){
     const breadth = document.getElementById('breadth').value;
     const height = document.getElementById('height').value;
@@ -18,7 +19,7 @@ document.getElementById('calculateTriangle').addEventListener('click' , function
     showResult.appendChild(section);
 })
 
-
+// -----Rectangle-------
 document.getElementById('calculateRectangle').addEventListener('click' , function(){
     const width = document.getElementById('width').value;
     const length = document.getElementById('length').value;
@@ -38,3 +39,27 @@ document.getElementById('calculateRectangle').addEventListener('click' , functio
     `
     showResult.appendChild(section);
 })
+
+// -------Parallelogram--------
+document.getElementById('calculateParallelogram').addEventListener('click' , function(){
+    const breadth = document.getElementById('paraValue1').value;
+    const height = document.getElementById('paraValue2').value;
+    const area = breadth * height;
+
+    // show value in calculation area
+    const showResult = document.getElementById('dynamicResults');
+    const section = document.createElement('section');
+    section.innerHTML = `
+       <ul class="calculationListStyling">
+         <li class="d-flex justify-content-between align-items-center">
+          <p>Parallelogram</p>
+          <p>${area}cm<sup>2</sup></p>
+          <button type="button" class="btn btn-primary">Convert to m<sup>2</sup></button>
+        </li>
+       </ul>
+    `
+    showResult.appendChild(section);
+})
+
+// --------Rohmbus---------
+
